23221c5d01b9bbf00b181b593de6f0e71e353876
EricThompson2 <dsm144@txstate.edu>
2026-04-05 17:57:38 -0500

    Added method to get control signals

--- CPU.py
+++ CPU.py
@@ -6,3 +6,21 @@ class CPU:
         self.__registerFile = registerFile()
         self.__ALU = ALU()
         self.__debug = debug
+
+        def getControlSignals(self, opCode):
+            control_signals = {
+                "ADD"  : {"RegDst": 1, "RegWrite" : 1, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 1, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+                "ADDI" : {"RegDst": 0, "RegWrite" : 1, "ALUSrc" : 1, "MemWrite" : 0, "MemtoReg" : 1, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+                "MUL"  : {"RegDst": 1, "RegWrite" : 1, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 1, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+                "SUB"  : {"RegDst": 1, "RegWrite" : 1, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 1, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+                "AND"  : {"RegDst": 1, "RegWrite" : 1, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 1, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+                "OR"   : {"RegDst": 1, "RegWrite" : 1, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 1, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+                "SLL"  : {"RegDst": 1, "RegWrite" : 1, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 1, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+                "SRL"  : {"RegDst": 1, "RegWrite" : 1, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 1, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+                "LW"   : {"RegDst": 0, "RegWrite" : 1, "ALUSrc" : 1, "MemWrite" : 0, "MemtoReg" : 0, "MemRead" : 1, "Branch" : 0, "Jump" : 0},
+                "SW"   : {"RegDst": 0, "RegWrite" : 0, "ALUSrc" : 1, "MemWrite" : 1, "MemtoReg" : 0, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+                "BEQ"  : {"RegDst": 0, "RegWrite" : 0, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 0, "MemRead" : 0, "Branch" : 1, "Jump" : 0},
+                "J"    : {"RegDst": 0, "RegWrite" : 0, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 0, "MemRead" : 0, "Branch" : 0, "Jump" : 1}, 
+                "NOP"  : {"RegDst": 0, "RegWrite" : 0, "ALUSrc" : 0, "MemWrite" : 0, "MemtoReg" : 0, "MemRead" : 0, "Branch" : 0, "Jump" : 0},
+            }
+            return control_signals.get(opCode)
