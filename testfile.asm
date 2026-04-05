d9725ed1a69ec773dad8899a9e6cf63a1fbf30a2
Cesarv48 <cesarvillarruel31@gmail.com>
2026-04-05 01:27:00 -0500

    Add files via upload
    test asm file created for testing

---
+++ testfile.asm
@@ -0,0 +1,19 @@
+# EXAMPLE ASM FILE FOR TESTING
+
+ADDI $t0, $zero, 5
+ADDI $t1, $zero, 3
+ADD  $t2, $t0, $t1
+SUB  $t3, $t0, $t1
+MUL  $t4, $t0, $t1
+AND  $t5, $t0, $t1
+OR   $t6, $t0, $t1
+SLL  $t7, $t0, 1
+SRL  $t8, $t0, 1
+SW   $t2, 0($zero)
+LW   $s0, 0($zero)
+BEQ  $t0, $t0, skip
+NOP
+skip:
+J    done
+done:
+NOP
