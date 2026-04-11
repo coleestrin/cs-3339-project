e56f4d1b5c28d2ebbc32fb6774c10aeebecaede5
EricThompson2 <dsm144@txstate.edu>
2026-04-10 19:31:20 -0500

    Added ALU functionality

--- Microarchitecture/ALU.py
+++ Microarchitecture/ALU.py
@@ -1,2 +1,29 @@
 class ALU:
-    pass
+
+    def __init__(self):
+        zero = True
+        result = 0
+
+    def run(self, opcode, operand1, operand2):
+        if opcode == "ADD" or opcode == "ADDI" or opcode == "LW" or opcode == "SW":
+            self.result = operand1 + operand2
+        elif opcode == "SUB" or opcode == "BEQ":
+            self.result = operand1 - operand2
+        elif opcode == "MUL":
+            self.result = operand1 * operand2
+        elif opcode == "AND":
+            self.result = operand1 & operand2
+        elif opcode == "OR":
+            self.result = operand1 | operand2
+        elif opcode == "SLL":
+            self.result = operand1 << operand2
+        elif opcode == "SRL":
+            self.result = operand1 >> operand2
+        
+        self.zero = (self.result == 0)
+
+    def getResult(self):
+        return self.result
+    
+    def zero(self):
+        return self.zero
