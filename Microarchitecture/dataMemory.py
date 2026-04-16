264bceb8abf07352617b5fc25b5f61753a7f3db7
wyvernofwhimsy <wyvernofwhimsy@gmail.com>
2026-04-15 22:41:25 -0500

    Added dataMemory and registerFile functionality
    dataMemory:
    create memory of variable size and starting base address: Memory(size,base_addr)
    read/write 8 bits and 32 bits:
    mem.read8(addr)
    mem.write8(addr, value)
    mem.read32(addr)
    mem.write32(addr, value)
    To run: python dataMemory.py
    
    registerFile:
    create 32 registers of size 32 bits:
    RegisterFile()
    read/write to register:
    reg_file.read(reg_num)
    reg_file.write(reg_num, value)

--- Microarchitecture/dataMemory.py
+++ Microarchitecture/dataMemory.py
@@ -1,2 +1,58 @@
-class dataMemory:
+class AddressError(Exception):
     pass
+
+class AlignmentError(Exception):
+    pass
+
+class Memory:
+    def __init__(self, size, base_addr):
+        self.size = size
+        self.base_addr = base_addr
+        self.byte_arr = bytearray(size)
+    
+    def __repr__(self):
+        return f"Memory(byte_arr = {self.byte_arr}, size = {self.size}, base_addr = {self.base_addr})"
+    
+    def check_valid_address(self, addr):
+        if addr > (self.base_addr + self.size) or addr < self.base_addr: 
+            raise AddressError(f"Invalid memory address: 0x{addr:08X}")
+    
+    def check_address_alignment(self, addr, size):
+        if (size == 2 and addr % 2 != 0):
+            raise AlignmentError(f"Halfword misaligned address: 0x{addr:08X}")
+        if (size == 4 and addr % 4 != 0):
+            raise AlignmentError(f"Word misaligned address: 0x{addr:08X}")
+    
+    def read8(self, addr):
+        self.check_valid_address(addr)
+        return self.byte_arr[addr]
+    
+    def write8(self, addr, value):
+        self.check_valid_address(addr)
+        self.check_valid_address(addr + 3)
+
+        self.byte_arr[addr] = value & 0xFF #puts only lowest 8 bits
+    
+    def read32(self, addr):
+        self.check_valid_address(addr)
+        self.check_valid_address(addr + 3)
+        self.check_address_alignment(addr, 4)
+
+        b0 = self.byte_arr[addr]
+        b1 = self.byte_arr[addr+1] << 8 #sll 8 for printing
+        b2 = self.byte_arr[addr+2] << 16
+        b3 = self.byte_arr[addr+3] << 24
+
+        #this prints LITTLE ENDIAN
+        return b0 | b1 | b2| b3
+    
+    def write32(self, addr, value):
+        self.check_valid_address(addr)
+        self.check_valid_address(addr+3)
+        self.check_address_alignment(addr, 4)
+
+        #this writes LITTLE ENDIAN
+        self.byte_arr[addr] = value & 0xFF
+        self.byte_arr[addr+1] =  (value >> 8) & 0xFF #srl 8 for writing next 8 to mem
+        self.byte_arr[addr+2] =  (value >> 16) & 0xFF
+        self.byte_arr[addr+3] =  (value >> 24) & 0xFF
--- Microarchitecture/registerFile.py
+++ Microarchitecture/registerFile.py
@@ -1,2 +1,33 @@
-class registerFile:
-    pass
+import numpy as np
+MASK32 = 0xFFFFFFFF
+
+REG_NAMES = (
+    "zero", "at", "v0", "v1", "a0", "a1", "a2", "a3",
+    "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
+    "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
+    "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra",
+)
+
+class RegisterFile:
+    _reg_array = np.zeros(32, dtype=np.int32)
+    
+    def reset(self):
+        for i in self._reg_array:
+            self._reg_array[i] = 0
+
+    def read(self, i):
+        if i > 32:
+            raise ValueError(f"Register {i} is not a valid register")
+        if i == 0:
+            return 0        
+        return self._reg_array[i]
+
+    def write(self, i, value):
+        if i == 0:
+            return 
+        self._reg_array[i] = value & MASK32 #manually limiting to 32 bits
+    
+    def dump(self):
+        for i in range(32):
+            print(f"${REG_NAMES[i]} ({i:02d}) = 0x{self.read(i):08X}")
+
