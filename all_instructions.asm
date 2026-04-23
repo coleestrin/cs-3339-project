c9a009f06242c32859f1dad8e5007eca91570b7d
wyvernofwhimsy <wyvernofwhimsy@gmail.com>
2026-04-22 21:57:28 -0500

    Re-updated to remove the .data section
    Put in the wrong file earlier, changing to the right one.

--- all_instructions.asm
+++ all_instructions.asm
@@ -1,22 +1,27 @@
-		.data
-		
-integer1:	.word 20
-integer2:	.word 4
-result:		.word 0
-
-
-		.text
 main:		
-		lw $t1, integer1
-		lw $t2, integer2
-		add $t0, $t0, $t1
+		#simulate .data section by simulating start of memory storing data
+		#set start of mem
+		addi $s0, $zero, 0
+		
+		addi $t9, $zero, 20
+		sw $t9, 0($s0) #integer1 stored in memory at 0
+		
+		addi $t8, $zero, 4
+		sw $t8, 4($s0) #integer2 stored in memory at 4
+		
+		and $t9, $t9, $zero #resets register $t9
+		and $t8, $t8, $zero #resets register $t8
+
+
+		lw $t1, 0($s0) #get integer1 and store in $t1
+		lw $t2, 4($s0) #get integer2 and store in $t2
+		add $t0, $t0, $t1 #$t0 is working register
 		mul $t0, $t0, $t1
-		div $t0, $t0, $t2
 		sub $t0, $t0, $t2
 		#checks if last bit is even
-		add $t3, $t3, 1 #sets t3 to 1 to check t4's result in next instruction	
+		addi $t3, $t3, 1 #sets t3 to 1 to check t4's result in next instruction	
 		and $t4, $t0, $t3 #sets t4 to either 0 or 1
-		beq $t0, $zero, is_even
+		beq $t4, $zero, is_even
 		nop
 		addi $t0, $t0, 1
 		j continue
@@ -27,10 +32,9 @@ continue:
 		sll $t0, $t0, 4 #mult by 2^4=16
 		or $t0, $t0, $t3 #forces t0 to be odd
 		
-		sw $t0, result	
+		sw $t0, 8($s0) #stores result in memory at 8 bytes from start
 		
 		
 exit:
-		li $v0, 10
-		syscall
+		nop
 
