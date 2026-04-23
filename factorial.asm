4659c6055b5a57d2e2507fdf7e3a394437909f5b
EricThompson2 <dsm144@txstate.edu>
2026-04-23 11:48:13 -0500

    Fixed inconsistent tabs and spaces

--- factorial.asm
+++ factorial.asm
@@ -1,48 +1,48 @@
-main:		
-		#simulate .data section by simulating start of memory storing data
-		#set start of mem
-		addi $s0, $zero, 0 
+main:        
+        #simulate .data section by simulating start of memory storing data
+        #set start of mem
+        addi $s0, $zero, 0 
 
-		addi $t9, $zero, 10
+        addi $t9, $zero, 10
         nop
         nop
-		sw $t9, 0($s0) #int stored in memory at 0
-		#answer will be in memory at 4 bytes from start
-		
-		and $t9, $t9, $zero #resets register $t9
-		
-		#get value to factorialize
-		lw $t0, 0($s0)
+        sw $t9, 0($s0) #int stored in memory at 0
+        #answer will be in memory at 4 bytes from start
+        
+        and $t9, $t9, $zero #resets register $t9
+        
+        #get value to factorialize
+        lw $t0, 0($s0)
         nop
-        nop	
-		beq $t0, $zero, base_case
-		nop
-        nop
-		addi $t1, $zero, 1
-        nop
-        nop	
-factorial_func:	#mult fac
-		mul $t1, $t1, $t0
-		addi $t0, $t0, -1
+        nop    
+        beq $t0, $zero, base_case
         nop
         nop
-
-		beq $t0, $zero, finish_fac
-		nop
+        addi $t1, $zero, 1
+        nop
+        nop    
+factorial_func:    #mult fac
+        mul $t1, $t1, $t0
+        addi $t0, $t0, -1
+        nop
         nop
 
-		j factorial_func
-		nop
+        beq $t0, $zero, finish_fac
         nop
-		
-base_case: 	#handle 0 by setting result to 1
-		addi $t1, $zero, 1
+        nop
+
+        j factorial_func
         nop
         nop
         
-		
-finish_fac:	#store result
-		sw $t1, 4($s0) #store answer in memory
-		
-		#end program
-		nop
+base_case:     #handle 0 by setting result to 1
+        addi $t1, $zero, 1
+        nop
+        nop
+        
+        
+finish_fac:    #store result
+        sw $t1, 4($s0) #store answer in memory
+        
+        #end program
+        nop
