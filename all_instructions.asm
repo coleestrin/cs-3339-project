2ca34f0dea60bf80d8ada2e7bfe6acd02a4848d7
wyvernofwhimsy <wyvernofwhimsy@gmail.com>
2026-04-22 23:36:28 -0500

    Added NOPs
    Added NOPs so the file is functional in the simulator.

--- all_instructions.asm
+++ all_instructions.asm
@@ -4,9 +4,11 @@ main:
 		addi $s0, $zero, 0
 		
 		addi $t9, $zero, 20
+
+		addi $t8, $zero, 4
+		nop
 		sw $t9, 0($s0) #integer1 stored in memory at 0
 		
-		addi $t8, $zero, 4
 		sw $t8, 4($s0) #integer2 stored in memory at 4
 		
 		and $t9, $t9, $zero #resets register $t9
@@ -15,23 +17,44 @@ main:
 
 		lw $t1, 0($s0) #get integer1 and store in $t1
 		lw $t2, 4($s0) #get integer2 and store in $t2
+		nop
 		add $t0, $t0, $t1 #$t0 is working register
+		nop
+		nop
 		mul $t0, $t0, $t1
+		nop
+		#checks if last bit is evenresult later
+		addi $t3, $t3, 1 #sets t3 to 1 to check t4's 
 		sub $t0, $t0, $t2
-		#checks if last bit is even
-		addi $t3, $t3, 1 #sets t3 to 1 to check t4's result in next instruction	
+		nop
+		nop
+		nop
 		and $t4, $t0, $t3 #sets t4 to either 0 or 1
+		nop
+		nop
+		nop
 		beq $t4, $zero, is_even
+		nop
+		nop
 		nop
 		addi $t0, $t0, 1
 		j continue
 		nop
+		nop
+		nop
 is_even:
+		nop
+		nop
 		srl $t0, $t0, 2 #div by 2^2=4
+		nop
+		nop
 continue:
 		sll $t0, $t0, 4 #mult by 2^4=16
+		nop
+		nop
 		or $t0, $t0, $t3 #forces t0 to be odd
-		
+		nop
+		nop
 		sw $t0, 8($s0) #stores result in memory at 8 bytes from start
 		
 		
