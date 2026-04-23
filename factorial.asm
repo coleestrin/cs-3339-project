9116115e9d578def85cdae28be8a156fa636029b
wyvernofwhimsy <wyvernofwhimsy@gmail.com>
2026-04-22 23:35:52 -0500

    Factorial with NOPS - not functional
    In the process of adding NOPs to factorial, but am encountering an infinite loop bug that I am having trouble detecting.

--- factorial.asm
+++ factorial.asm
@@ -2,8 +2,10 @@ main:
 		#simulate .data section by simulating start of memory storing data
 		#set start of mem
 		addi $s0, $zero, 0 
-		
 		addi $t9, $zero, 5
+		nop
+		nop
+		nop
 		sw $t9, 0($s0) #int stored in memory at 0
 		#answer will be in memory at 4 bytes from start
 		
@@ -11,26 +13,50 @@ main:
 		
 		#get value to factorialize
 		lw $t0, 0($s0)
+		nop
+		nop
+		nop
 		
 		beq $t0, $zero, base_case
 		nop
 		addi $t1, $zero, 1
+		nop
+		nop
+		nop
 		
 factorial_func:	#mult fac
+		# t1 = t1 * t0
 		mul $t1, $t1, $t0
-		addi $t0, $t0, -1
+		nop
+		nop
+		nop
+
+		# t2 = 1
+		addi $t2, $zero, 1
+		nop
+		nop
+		nop
+
+		# t0 = t0 - t2
+		sub $t0, $t0, $t2
+		nop
+		nop
+		nop
+
+		# check if t0 == 0 (now safe to read updated t0)
 		beq $t0, $zero, finish_fac
 		nop
 		j factorial_func
 		nop
 		
 base_case: 	#handle 0 by setting result to 1
-		addi $t0, $zero, 1
+		addi $t1, $zero, 1
+		nop
+		nop
+		nop
 		
 finish_fac:	#store result
 		sw $t1, 4($s0) #store answer in memory
 		
 		#end program
 		nop
-		
-		
